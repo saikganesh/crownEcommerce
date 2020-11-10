c0d87c61eea062c7ff3c4638c96ccc30dfcb8e93
saikganesh <saikrishnan2469@gmail.com>
2020-11-09 18:50:05 -0800

    No Change

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ app.use(cors())
 if(process.env.Node_ENV === 'production')
 {
     
-    app.use(express.static(path.join(__dirname,'client/build')))
+    app.use(express.static(path.join(__dirname,'./client/build')))
     
     
     app.get('*',(req,res)=>{
-        res.sendFile(path.join( __dirname,'client/build','index.html')) 
+        res.sendFile(path.join( __dirname,'./client/build','index.html')) 
     })
 }
 
